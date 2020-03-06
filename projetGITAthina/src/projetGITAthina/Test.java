fe83af7597e4ddb44a4332bdd7e3523901a020fa
Athina HICKS <athina@yahoo.fr>
2020-03-06 10:09:30 +0100

    V1 projet athina

---
+++ projetGITAthina/src/projetGITAthina/Test.java
@@ -0,0 +1,11 @@
+package projetGITAthina;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Projet Athina GIT V1");
+	}
+
+}
